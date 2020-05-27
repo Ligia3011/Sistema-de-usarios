6a845faf2e71a5774a9a376839492964a009ea8c
Ligia3011 <62628152+Ligia3011@users.noreply.github.com>
2020-05-27 14:35:22 -0600

    :(

---
+++ menu.php
@@ -0,0 +1,7 @@
+<? php
+< select  name = " operacion " >
+                < option  value = " 1 " > Crear usuario </ option >
+                < option  value = " 2 " > Visualizar usuarios </ option >
+                < option  value = " 3 " > Salir </ option >
+                
+            </ select >
